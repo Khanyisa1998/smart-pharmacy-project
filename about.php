49273e19f8c0191831627b3d14be44e368138d0b
Khanyisa1998 <124432104+Khanyisa1998@users.noreply.github.com>
2023-02-05 15:02:24 +0200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,118 @@
+<?php include 'header.php';?>
+<!-- breadcrumb start-->
+    <section class="breadcrumb_part breadcrumb_bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumb_iner">
+                        <div class="breadcrumb_iner_item">
+                            <h2>about us</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- breadcrumb start-->
+<!-- about us part start-->
+    <section class="about_us single_about_padding">
+        <div class="container">
+            <div class="row justify-content-between align-items-center">
+                <div class="col-md-6 col-lg-6">
+                    <div class="about_us_img">
+                        <img src="img/top_service.png" alt="">
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-5">
+                    <div class="about_us_text">
+                        <h2>About us</h2>
+                        <h6>Proposed System System name: Smart Pharmacy</h6>
+                        <p>Patient subsystem
+                        The patient will be able to create their account, order over the counter medication and the system will be able to track the patient location so that it can choose the nearest pharmacy, the patient can then also be able to enter symptoms and get suggested medications to choose from based on the the symptoms entered by the patient and then if the entered symptoms requires medical attention the system must advice the patient to seek medication attention and also suggest  a nearby doctor</p>
+
+                        <p>Doctor Subsystem
+                        The doctor will be able to create their account; they will be able to write prescription for 
+                        their patients on the system using their handwriting on the device then the system must 
+                        then convert the device handwritten prescription to text and then make an order and attach 
+                        the converted prescription to the nearest pharmacy.</p>
+                        <a class="btn_2 " href="login.php">:Log In</a>
+                        <div class="banner_item">
+                            <div class="single_item">
+                                <img src="img/icon/banner_1.svg" alt="">
+                                <h5>Emergency</h5>
+                            </div>
+                            <div class="single_item">
+                                <img src="img/icon/banner_2.svg" alt="">
+                                <h5>Prescriptions</h5>
+                            </div>
+                            <div class="single_item">
+                                <img src="img/icon/banner_3.svg" alt="">
+                                <h5>Qualfied</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- about us part end-->
+    <!-- our depertment part start-->
+    <section class="our_depertment section_padding">
+        <div class="container">
+            <div class="row justify-content-center text-center">
+                <div class="col-xl-12">
+                    <div class="depertment_content">
+                        <div class="row justify-content-center">
+                            <div class="col-xl-8">
+                                <h2>Mission and Vision</h2>
+                                <div class="row">
+                                    <div class="col-lg-6 col-sm-6">
+                                        <div class="single_our_depertment">
+                                            <span class="our_depertment_icon"><img src="img/icon/feature_2.svg"
+                                                    alt=""></span>
+                                            <h4>Mission</h4>
+                                            <p>Darkness multiply rule Which from without life creature blessed
+                                                give moveth moveth seas make day which divided our have.</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-sm-6">
+                                        <div class="single_our_depertment">
+                                            <span class="our_depertment_icon"><img src="img/icon/feature_2.svg"
+                                                    alt=""></span>
+                                            <h4>Vision</h4>
+                                            <p>Darkness multiply rule Which from without life creature blessed
+                                                give moveth moveth seas make day which divided our have.</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-sm-6">
+                                        <div class="single_our_depertment">
+                                            <span class="our_depertment_icon"><img src="img/icon/feature_2.svg"
+                                                    alt=""></span>
+                                            <h4>Better Future</h4>
+                                            <p>Darkness multiply rule Which from without life creature blessed
+                                                give moveth moveth seas make day which divided our have.</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-sm-6">
+                                        <div class="single_our_depertment">
+                                            <span class="our_depertment_icon"><img src="img/icon/feature_2.svg"
+                                                    alt=""></span>
+                                            <h4>Better Future</h4>
+                                            <p>Darkness multiply rule Which from without life creature blessed
+                                                give moveth moveth seas make day which divided our have.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- our depertment part end-->
+
+
+
+<?php include 'footer.php';?>
